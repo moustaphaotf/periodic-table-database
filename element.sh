7347c41484cf46fe5af7d621c9cc20dbcfe94596
Mamadou Moustapha  Diallo <moustaphaotf@gmail.com>
2024-03-15 20:54:47 +0000

    feat: query database

--- element.sh
+++ element.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # if no argument is given
 if [[ ! $1 ]]
 then
   echo -e "Please provide an element as an argument."
+# else if first argument is an integer
+  exit
 elif [[ $1 =~ ^[0-9]+$ ]]
-  # if first argument is an integer
 then
-  echo true
-  # look for check by the atomic number
+  # check by the atomic number
+  ELEMENT_RESULT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
 else
-  echo false
+  ELEMENT_RESULT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol ILIKE '$1' OR name ILIKE '$1'")
   # if not, check by the symbol or element name
 fi
-  
 
+# if the element exists
+echo $ELEMENT_RESULT
 
