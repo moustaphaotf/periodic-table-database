f7e2f6aeae93a4d62950e4294433c04bb0c7d8f7
Mamadou Moustapha  Diallo <moustaphaotf@gmail.com>
2024-03-15 20:30:16 +0000

    feat: program arguments

--- element.sh
+++ element.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo -e "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+# if no argument is given
+if [[ ! $1 ]]
+then
+  echo -e "Please provide an element as an argument."
+elif [[ $1 =~ ^[0-9]+$ ]]
+  # if first argument is an integer
+then
+  echo true
+  # look for check by the atomic number
+else
+  echo false
+  # if not, check by the symbol or element name
+fi
+  
+
+
